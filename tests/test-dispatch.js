3b97325582f5e8ca1d13b4732bd264667f253d78
add0n <bedney@yahoo-inc.com>
2012-10-15 10:09:56 -0700

    Initial framing with simple test

---
+++ tests/test-dispatch.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2011-2012, Yahoo! Inc.  All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+YUI().use('mojitoDriver', 'testMojit1', 'test', 'app', function (Y) {
+
+    var suite = new Y.Test.Suite('mojito-dispatch tests'),
+        A = Y.Assert,
+        OA = Y.ObjectAssert,
+
+        msg;
+
+    suite.add(new Y.Test.Case({
+
+        name: 'instantiation',
+
+        setUp: function() {
+            var app = new Y.App();
+            Y.appObj = app;
+            Y.mojito.mojitoDriver.init();
+        
+            msg = Y.one('#Message');
+        },
+
+        tearDown: function() {
+        },
+
+        'test route navigation function': function() {
+            Y.appObj.navigate('/testMojit1:index');
+
+            Y.Assert.areEqual(msg.get('text'), ' || Navigated to: http://localhost/testMojit1:index');
+        }
+
+    }));
+
+    Y.Test.Runner.add(suite);
+});
